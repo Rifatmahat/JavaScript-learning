cd335212ada98879cebcd9ca170ac968a9823353
Rifatmahat <rifatmahat97@gmail.com>
2022-10-04 08:30:58 +0530

    new

---
+++ demo.js
@@ -0,0 +1,6 @@
+// This is my first program.
+console.log("Hello");
+console.log('hii');
+console.info("This is info"); //info=information
+console.warn("error"); //warn=warning
+console.error("OOps");
