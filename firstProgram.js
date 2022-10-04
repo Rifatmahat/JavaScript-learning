dcfa3aea4d8911d3d694ebb6c79c0d0b45bbc8de
Rifatmahat <rifatmahat97@gmail.com>
2022-10-04 20:02:16 +0530

    Created first js file
    Created first js file.

---
+++ firstProgram.js
@@ -0,0 +1,10 @@
+//first program in javascript
+console.log("Hello Everyone...")
+console.log("Myself Rifat Mahat");
+console.log("Hobbies: Reading,Writing");
+console.log("Strength: Quick Learner, Hard worker");
+console.warn("Weakness: Sensitive, Over Thinker");
+console.log("Addition of 30+50:");
+console.log(30+50);
+console.log("Multiplication of 49+479:");
+console.log(49*479);
