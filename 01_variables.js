461a702094642c4baa66e949319ef45527e1d107
Rifatmahat <rifatmahat97@gmail.com>
2022-10-06 08:24:51 +0530

    updated

---
+++ 01_variables.js
@@ -0,0 +1,11 @@
+
+var my_apple;       //variable declaration
+my_apple = "Apple";     //variable initialization
+
+var my_name = "Rifat Mahat";        //variable declaration and initialization on same line
+my_name = "Rifat R Mahat";
+console.log(my_name);
+console.log(my_apple);
+
+my_apple = "200";
+console.log(my_apple);
