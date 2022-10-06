98657bbf8c32ee2e60bf1a0afb16c94381503c56
Rifatmahat <rifatmahat97@gmail.com>
2022-10-06 10:13:20 +0530

    created swapping assignment
    created swapping assignment of js

---
+++ 01_assign1_swapping_var.js
@@ -0,0 +1,12 @@
+var myNumber;
+myNumber=100;
+    console.log(myNumber);
+
+var myLove = "I Love Only JS";
+    console.log("Declaration & Initialization in one line: ",myLove);
+
+var temp = 200;
+    console.log("Before updating:",temp);
+
+temp="JavaScript";
+    console.log("After Updating the 'temp' variable:",temp);
