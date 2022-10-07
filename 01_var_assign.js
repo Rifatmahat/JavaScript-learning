d8d5c7f539f3d12da6ddda5cbfed3c462fdb9c68
Rifatmahat <rifatmahat97@gmail.com>
2022-10-07 12:00:02 +0530

    new assignment of js variables
    new assignment of js variables

---
+++ 01_var_assign.js
@@ -0,0 +1,31 @@
+
+var myFullName;
+myFullName="Rifat Mahat";
+console.log("1. The initial value:",myFullName);
+myFullName= "Rifat R Mahat";
+console.log("2. Updated values:",myFullName);
+
+var myLove="I Love JS";
+console.log(myLove);
+var firstName= "Rifat";
+console.log("First Name:",firstName);
+
+var lastname="Mahat";
+console.log("Last Name:",lastname);
+
+var age="25";
+console.log("Age:",age)
+
+var address ="Shivaji Road,Miraj";
+console.log("Address:",address);
+
+var pin ="416410";
+console.log("POin Code:",pin);
+
+var gender ="Female";
+console.log("Gender:",gender);
+
+var status ="No";
+console.log("Marital status:",status);
+
+
