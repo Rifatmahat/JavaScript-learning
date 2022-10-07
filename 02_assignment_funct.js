3aeba59f7f5c9b3cde0f26c539dd8aeb35a8ce16
Rifatmahat <rifatmahat97@gmail.com>
2022-10-07 17:51:16 +0530

    Create 02_assignment_funct.js

---
+++ 02_assignment_funct.js
@@ -0,0 +1,16 @@
+//swap the two numbers or string.....
+var arg3=
+function swap_values(arg3,arg4);
+{
+    console.log("------Before Swapping-------");
+    console.log(arg3,arg4);
+    var temp=arg3;
+    arg3=arg4;
+    arg4=temp;
+
+    console.log("--------After Swapping-------");
+    console.log(arg3,arg4);
+}
+
+swap_values("Rifat","Mahat");
+swap_values("1111","0000")
