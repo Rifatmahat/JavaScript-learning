12221af493c221e757089f7888db9113a818304e
Rifatmahat <rifatmahat97@gmail.com>
2022-10-07 17:30:15 +0530

    created assignment of datatypes
    created assignment of datatypes.

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+//the different type of datatypes
+console.log("**********Assignment 01***********");
+
+var pin=416410;
+console.log(pin,typeof pin);;
+console.log("*********************");
+
+var fname="rifat";
+console.log(fname,typeof fname);
+console.log("*********************");
+
+var isMarried= false;
+console.log(isMarried,typeof isMarried);
+console.log("*********************");
+
+//chech the value null id or undefined
+
+var lname;
+console.log(lname,typeof lname);
+console.log("*********************");
+
+var age=25;
+console.log(age,typeof age);
+
+
