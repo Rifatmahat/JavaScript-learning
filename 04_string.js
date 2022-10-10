4e30e758ccf161b10b7f0aaab835e98e2a80415d
Rifatmahat <rifatmahat97@gmail.com>
2022-10-10 08:59:38 +0530

    js string

---
+++ 04_string.js
@@ -0,0 +1,20 @@
+
+var greet="Good Morning sis and bro";
+greet.length;
+
+console.log(greet);
+
+var count=greet.length;
+console.log(count);
+
+console.log(greet.length);
+
+var rifat=greet.charAt(8);
+console.log(rifat);
+
+console.log(greet.charAt(8));
+
+var char=greet.indexOf('0');
+console.log(char);
+
+console.log(greet.indexOf("bro"));
