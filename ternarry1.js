3a8f086fd0bf510de11ed70e5185725d3e428929
Rifatmahat <rifatmahat97@gmail.com>
2022-10-14 13:48:07 +0530

    created ternary file in js

---
+++ ternarry1.js
@@ -0,0 +1,9 @@
+var DiageoEligibilityCheck = function (grad_score, hsc_score, ssc_score, candidate_name) {
+    console.log(grad_score >= 70 );
+        var result = grad_score >= 70 || hsc_score >= 80 || ssc_score > 90  // false || true
+            ? `Congrates ${candidate_name} you are eligible for Diageo interview`
+            : "Unfortunately you are not eligible for interview";
+        console.log(result);
+    }
+    DiageoEligibilityCheck(80, 86, 90, "Rifat Mahat");
+    DiageoEligibilityCheck(69, 65, 95, "M Rifat");
